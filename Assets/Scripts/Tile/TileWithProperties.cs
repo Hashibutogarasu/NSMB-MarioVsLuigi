8938e2cc0b9a41d91cb4952ca22ca2f6348da717
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2024-06-15 01:01:32 +0900

    Added Beach Stage

--- Assets/Scripts/Tile/TileWithProperties.cs
+++ Assets/Scripts/Tile/TileWithProperties.cs
@@ -2,6 +2,6 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "TileWithProperties", menuName = "ScriptableObjects/Tiles/TileWithProperties", order = 3)]
 public class TileWithProperties : SiblingRuleTile {
-    public bool isBackgroundTile = false, iceSkidding = false;
+    public bool isBackgroundTile = false, iceSkidding, isSwimable = false;
     public Enums.Sounds footstepSound = Enums.Sounds.Player_Walk_Grass;
 }
